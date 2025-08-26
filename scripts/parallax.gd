3a1e6345562cb3b48e09ff5d128ea6a050e6bc45
Aksel Hjerpbakk <akselhallajs@gmail.com>
2025-08-26 20:22:14 +0200

    improve hell scene and shaders

--- scripts/parallax.gd
+++ scripts/parallax.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 @onready var player = get_tree().get_nodes_in_group("Player")[0]  
 
-@export_range(1, 10, 0.5, "How slow or fast the background should move relative to the player")
-var distance: float = 1
+@export_range(-5, 5, 0.01, "How slow or fast the background should move relative in x to the player")
+var distance_x: float = 1
+
+@export_range(-5, 5, 0.01, "How slow or fast the background should move relative in y to the player")
+var distance_y: float = 1
+
 
 func _physics_process(delta: float) -> void:
-	position.x -= (player.velocity.x / distance) * delta
-	position.y -= (player.velocity.y / distance) * delta
+	position.x -= (player.velocity.x * distance_x) * delta
+	position.y -= (player.velocity.y * distance_y) * delta
