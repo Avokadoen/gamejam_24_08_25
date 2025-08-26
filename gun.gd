d31e139fe957c3af7ae3421f4445485198434034
mikael-rh <hanssen.mikael@gmail.com>
2025-08-26 19:46:34 +0200

    Update gun to fire on key press

--- gun.gd
+++ gun.gd
@@ -3,14 +3,16 @@ extends Node2D
 @onready var main = get_tree().get_root().get_node("Node2D");
 @onready var projectile = load("res://projectile.tscn")
 
-func _ready() -> void:
-	while 100 > 0:
+@export var muzzle_dist = 10
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
 		shoot()
-		await get_tree().create_timer(1.0).timeout
 	
 func shoot():
 	var instance = projectile.instantiate()
-	instance.dir = rotation
-	instance.startPos = global_position
+	var dir = Vector2(1, 0).rotated(rotation)
+	instance.dir = dir
+	instance.startPos = global_position + dir * muzzle_dist
 	instance.startRot = rotation
 	main.add_child.call_deferred(instance)
--- projectile.gd
+++ projectile.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 @export var SPEED = 100
 
-var dir : float
+var dir : Vector2
 var startPos : Vector2
 var startRot : float
 
 func _ready() -> void:
 	global_position = startPos
 	global_rotation = startRot
+	velocity = SPEED * dir
 	
 func _physics_process(_delta: float) -> void:
-	velocity = Vector2(0, -SPEED).rotated(dir)
 	move_and_slide()
