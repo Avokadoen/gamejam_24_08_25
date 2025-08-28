88075c8484423f7fbbb03b4d24ae0cef9922442e
JoJo <49401090+JayJayDev@users.noreply.github.com>
2025-08-28 22:43:08 +0200

    Death + jump animation
    Death animation plays when the player dies.
    The start of the jump animation plays when you press jump, but it doesn't go back to running. Needs fixing.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 class_name Player
 
+@onready var anim_player = $AnimatedSprite2D
+
 const SPEED = 600.0
 const JUMP_VELOCITY = -300.0
 
@@ -19,6 +21,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim_player.play("Jump_Start")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -28,9 +31,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+
 	move_and_slide()
 	
 func takeDamage(damage: float) -> void:
 	health -= damage
 	if (health <= 0):
+		anim_player.play("Death")
 		print("player is dead, long live the player")
