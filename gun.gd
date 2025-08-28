eac43f857d5940543ab12f1eaa7497746a2fa133
Torbjørn Økland <torb1997@gmail.com>
2025-08-28 20:23:17 +0200

    The logic for enemy gun

--- gun.gd
+++ gun.gd
@@ -1,18 +1,37 @@
 extends Node2D
 
 @onready var main = get_tree().get_root().get_node("Node2D");
-@onready var projectile = load("res://projectile.tscn")
+## Bullet Scene/Prefab
+@export var bullet_scene: PackedScene
 
+##Distance between gun and muzzle
 @export var muzzle_dist = 10
-	
+
+## If this gun is equipped by an enemy
+@export var enemyGun: bool
+## The target the enemy is shooting at
+@export var target: Node2D
+## The interval the enemy is shooting at
+@export var interval: float = 0.6
+## The speed of the bullet
+@export var bullet_speed: float = 900.0
+
+var _timer := 0.0
+
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
+	if enemyGun:
+		_timer += delta
+		if _timer >= interval:
+			_timer = 0.0
+			shoot()
+	elif Input.is_action_just_pressed("shoot"):
 		shoot()
 	
 func shoot():
-	var instance = projectile.instantiate()
-	var dir = Vector2(1, 0).rotated(rotation)
+	var instance = bullet_scene.instantiate()
+	var dir: Vector2 = ((target.global_position - global_position).normalized()) if target else (Vector2(1, 0).rotated(rotation))
 	instance.dir = dir
 	instance.startPos = global_position + dir * muzzle_dist
 	instance.startRot = rotation
+	instance.SPEED = bullet_speed
 	main.add_child.call_deferred(instance)
