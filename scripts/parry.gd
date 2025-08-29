e73e3b74913ccf28b0d0c5916b88af652a2d3684
mikael-rh <hanssen.mikael@gmail.com>
2025-08-29 22:41:48 +0200

    Implement catch/release mechanic

--- scripts/parry.gd
+++ scripts/parry.gd
@@ -2,11 +2,15 @@ extends Area2D
 
 @export var duration_msec: int
 @export var cooldown_msec: int
+@export var capacity: int
 
 var last_parry_msec = 0
 
+var projectiles = []
+
 func _ready() -> void:
 	deactivate()
+	pass
 
 func _process(delta: float) -> void:
 	var elapsed_msec = Time.get_ticks_msec()
@@ -15,9 +19,14 @@ func _process(delta: float) -> void:
 	if monitoring:
 		if since_last_msec >= duration_msec:
 			deactivate()
-	elif since_last_msec >= cooldown_msec and Input.is_action_just_pressed("parry"):
-		last_parry_msec = elapsed_msec
-		activate()
+	elif Input.is_action_just_pressed("catch"):
+		if since_last_msec >= cooldown_msec and len(projectiles) < capacity:
+			last_parry_msec = elapsed_msec
+			activate()
+	elif Input.is_action_just_pressed("release"):
+		for projectile in projectiles:
+			release_projectile(projectile)
+		projectiles.clear()
 
 func activate() -> void:
 	monitoring = true
@@ -26,3 +35,28 @@ func activate() -> void:
 func deactivate() -> void:
 	monitoring = false
 	print("Parry off")
+
+func catch_projectile(projectile: Projectile) -> void:
+	if len(projectiles) >= capacity or projectiles.has(projectile):
+		return
+	
+	projectile.SPEED = 0
+	projectiles.append(projectile)
+	projectile.reparent.call_deferred(self)
+	projectile.collision_layer = 8
+	projectile.dir = Vector2.RIGHT
+	projectile.global_rotation_degrees = -90
+	print("Catch projectile")
+
+func release_projectile(projectile: Projectile) -> void:
+	var parent = get_tree().get_root()
+	projectile.reparent.call_deferred(parent)
+	projectile.SPEED = 500
+	print("Release projectile")
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Projectile:
+		catch_projectile(area)
+
+func _on_body_entered(body: Node2D) -> void:
+	pass
