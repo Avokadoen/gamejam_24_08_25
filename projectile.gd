d7d88a28f3638f7724d744a7287d3d4f7f69fb9f
Aksel Hjerpbakk <akselhallajs@gmail.com>
2025-08-30 16:10:17 +0200

    ensure we clean up projectiles

--- projectile.gd
+++ projectile.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 
-@export var SPEED = 100
+@export var speed = 100
+@export var lifetime = 5
 
-var dir : Vector2
-var startPos : Vector2
-var startRot : float
+var dir: Vector2
+var startPos: Vector2
+var startRot: float
+
+var timer: Timer
 
 func _ready() -> void:
 	global_position = startPos
 	global_rotation = startRot
-	velocity = SPEED * dir
+	velocity = speed * dir
+	timer = get_node("Timer")
+	timer.start(lifetime)
 	
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	queue_free()
