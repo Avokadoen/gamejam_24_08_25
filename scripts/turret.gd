8a462ddf93c94e31f9748d16185039b49a73ad85
mikael-rh <hanssen.mikael@gmail.com>
2025-08-30 15:52:04 +0200

    In turret, export projectile_speed and interval_sec

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -4,10 +4,12 @@ extends Node2D
 @onready var projectile = load("res://scenes/projectile.tscn")
 
 @export var timer: Timer
-@export var count = 1
-@export var rot_degrees_speed = 0
-@export var max_angle_degrees = 360
-@export var start_rot_offset_degrees = 0
+@export var count: int = 1
+@export var interval_sec: float = 1
+@export var projectile_speed: float = 100
+@export var rot_degrees_speed: float = 0
+@export var max_angle_degrees: float = 360
+@export var start_rot_offset_degrees: float = 0
 
 func _ready() -> void:
 	if timer != null:
@@ -21,11 +23,12 @@ func _on_timer_timeout() -> void:
 	for i in range(count):
 		var angle_degrees = max_angle_degrees * i / count
 		shoot(angle_degrees + global_rotation_degrees)
-	timer.start()
+	timer.start(interval_sec)
 		
 func shoot(angle_degrees):
 	var instance = projectile.instantiate()
 	var dir = Vector2(1, 0).rotated(angle_degrees * PI / 180)
+	instance.SPEED = projectile_speed
 	instance.dir = dir
 	instance.startPos = global_position
 	instance.startRot = (angle_degrees + start_rot_offset_degrees) * PI / 180
